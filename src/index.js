09943913c24fa1c830b2c16b93b804aac5950690
madura <madura@sqrmobile.com>
2017-10-28 21:29:33 +0530

    Fixed issues with react create class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React,{PureComponent} from 'react';
 import './main.css';
-export default React.createClass({
+export default class GoogleMaterilIcons extends PureComponent{
   render() {
     return  <i className="material-icons"
                       style={{fontSize: this.props.size||24}}>{this.props.icon}</i>;
   }
-})
+};
